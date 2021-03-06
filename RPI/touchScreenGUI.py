29abcb655c22ed11057a4e2381d1ce47296ff0d3
steveny9911 <hunglnl01@gmail.com>
2021-03-05 19:49:03 -0800

    touchscreen GUI update

--- RPI/touchScreenGUI.py
+++ RPI/touchScreenGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import requests
 from PIL import ImageTk, Image
 from tkinter import Text
 import os
@@ -15,6 +16,16 @@ frame.place(relwidth=0.75, relheight=0.75, relx = 0.1, rely = 0.1)
 label = tk.Label(frame, bg = "white", text = "Ingredients Decoder")
 label.config(font=('helvetica', 25))
 label.pack()
+
+# get user's list
+URL = "http://52.138.39.36:3000/plist"
+userName = 'customer1'
+PARAMS = {'username': 'customer1'}
+userList = requests.post(url = URL, json = PARAMS)
+print(userList.json())
+
+yourList = tk.Button(frame, text = "Your personalized list", padx = 10, pady = 5, fg = "white", bg = "black")
+yourList.pack()
 
 takePic = tk.Button(frame, text = "Check ingredients", padx = 10, pady = 5, fg = "white", bg = "black")
 takePic.pack()
