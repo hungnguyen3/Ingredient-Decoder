db1c0e753187e3b94de4f022ff5d8827dd0d5b3c
steveny9911 <hunglnl01@gmail.com>
2021-03-09 01:27:36 -0800

    add gitignore and work on GUI desgin

--- RPI/pageTransition.py
+++ RPI/pageTransition.py
@@ -1,71 +1,88 @@
 import tkinter as tk
+import requests
 
-#extend the Tk of tkinter
 class App(tk.Tk):
-	def __init__(self, *args, **kwargs):
-		tk.Tk.__init__(self, *args, **kwargs)
-		#Setup Menu
-		MainMenu(self)
-		#Setup Frame
-		container = tk.Frame(self)
-		container.pack(side="top", fill="both", expand=True)
-		container.grid_rowconfigure(0, weight=1)
-		container.grid_columnconfigure(0, weight=1)
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.attributes('-fullscreen', True)
 
-		self.frames = {}
+        canvas = tk.Canvas(self, bg = "#263D42")
+        canvas.pack(fill = tk.BOTH, expand = True)
 
-		for F in (LandingPage, RegularItem, CustomItems):
-			frame = F(container, self)
-			self.frames[F] = frame
-			frame.grid(row=0, column=0, sticky="nsew")
+        #Set up Menu
+        MainMenu(self)
+        #Set up Frames
+        container = tk.Frame(canvas)
+        container.place(relwidth=0.75, relheight=0.75, relx = 0.1, rely = 0.1)
+        #container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0,weight=1)
 
-		self.show_frame(LandingPage)	
-	def show_frame(self, context):
-		frame = self.frames[context]
-		frame.tkraise()
+        self.frames = {}
+
+        for F in (LandingPage, RegularItems, CustomItems):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0,sticky="nsew")
+
+        self.show_frame(LandingPage)
+    def show_frame(self, context):
+        frame = self.frames[context]
+        frame.tkraise()
+    def show_plist(self, context):
+        URL = "http://52.138.39.36:3000/plist"
+        userName = 'customer1'
+        PARAMS = {'username': 'customer1'}
+        userList = requests.post(url = URL, json = PARAMS)
+        user_list = tk.Label(self.frames[context], text = userList.json())
+        user_list.pack(padx=10, pady=10)
 
 class LandingPage(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-		label = tk.Label(self, text="Please pick types of items to scan")
-		label.pack(padx=10, pady=10)
-		page_one = tk.Button(self, text="Regular Items", command=lambda:controller.show_frame(RegularItem))
-		page_one.pack()
-		page_two = tk.Button(self, text="Store Custom Items", command=lambda:controller.show_frame(CustomItems))
-		page_two.pack()
+        label = tk.Label(self, text = "Welcome to Ingredients Decoder")
+        label.config(font=('helvetica', 25))
+        label.pack(padx=10, pady=10)
+        regular_page = tk.Button(self, text="Regular Items", command=lambda:controller.show_frame(RegularItems))
+        regular_page.pack()
+        custom_page = tk.Button(self, text="Custom Items", command=lambda:controller.show_frame(CustomItems))
+        custom_page.pack()
+        user_list = tk.Button(self, text="View personalized list", command=lambda:controller.show_plist(LandingPage))
+        user_list.pack()
 
+class RegularItems(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-class RegularItem(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text = "Scan regular items here")
+        label.config(font=('helvetica', 25))
+        label.pack(padx=10, pady=10)
 
-		label = tk.Label(self, text="Scan regular items here")
-		label.pack(padx=10, pady=10)
-        start_page = tk.Button(self, text="Back to Home Page", command=lambda:controller.show_frame(LandingPage))
+        scan_items = tk.Button(self, text = "Check Ingredients")
+        scan_items.pack()
+        start_page = tk.Button(self, text = "Back to Home Page", command=lambda:controller.show_frame(LandingPage))
         start_page.pack()
-        page_two = tk.Button(self, text="Check Ingredients", command=lambda:controller.show_frame(CustomItems))
-        page_two.pack()
 
 class CustomItems(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-		label = tk.Label(self, text="Page Two")
-		label.pack(padx=10, pady=10)
-		start_page = tk.Button(self, text="Start Page", command=lambda:controller.show_frame(LandingPage))
-		start_page.pack()
-		page_one = tk.Button(self, text="Page One", command=lambda:controller.show_frame(RegularItem))
-		page_one.pack()
+        label = tk.Label(self, text = "Scan store custom items here")
+        label.config(font=('helvetica', 25))
+        label.pack(padx=10, pady=10)
+        scan_items = tk.Button(self, text = "Check Ingredients")
+        scan_items.pack()
+        start_page = tk.Button(self, text = "Back to Home Page", command=lambda:controller.show_frame(LandingPage))
+        start_page.pack()
 
 class MainMenu:
-	def __init__(self, master):
-		menubar = tk.Menu(master)
-		filemenu = tk.Menu(menubar, tearoff=0)
-		filemenu.add_command(label="Exit", command=master.quit)
-		menubar.add_cascade(label="File", menu=filemenu)
-		master.config(menu=menubar)
-
+    def __init__(self, master):
+        menubar = tk.Menu(master)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command=master.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        master.config(menu=menubar)
 
 app = App()
 app.mainloop()
