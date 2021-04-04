6004f856a7222a7248128e952d9730864662bf50
steveny9911 <hunglnl01@gmail.com>
2021-04-03 17:18:48 -0700

    bluetooth

--- RPI/Camera/camera2.py
+++ RPI/Camera/camera2.py
@@ -3,13 +3,18 @@ import os
 import RPi.GPIO as GPIO
 import time
 import serial
+import multiprocessing as mp
 
 ser = serial.Serial('/dev/ttyAMA0', 115200)
+#ser = serial.Serial('/dev/rfcomm0', 115200)
 if ser.isOpen == False:
     ser.open()
 
 path = os.path.dirname(os.path.abspath(__file__))
 imgPath = os.path.dirname(path) + "/images"
+
+#customerIdQueue = mp.Queue()
+#loginSuccessQueue = mp.Queue()
 
 def bluetoothLogin():
     username = []
@@ -18,7 +23,7 @@ def bluetoothLogin():
         if size != 0:
             response = ser.read(1)
             username.append(response)
-            #print(response)
+            print(response)
             if response == b"1" or response == b"2":
                 #print("yes, it is 1 end or 2 end")
                 break
@@ -26,19 +31,16 @@ def bluetoothLogin():
     #print("here")
     str1 = ""
     for element in username:
-        str1 += element
+        str1 += element.decode("utf-8")
     return str1
 
 def run(customerIdQueue, loginSuccessQueue):
     while(1):
+        str1 = bluetoothLogin()
         loginSuccessQueue.put(True)
-        customerIdQueue.put("customer2")
-        time.sleep(15)
-        loginSuccessQueue.put(True)
-        customerIdQueue.put("customer1")
-        time.sleep(15)
+        customerIdQueue.put(str1)
 
 
-#run(customerId, loginSuccess)
+#run(customerIdQueue, loginSuccessQueue)
 #bluetoothLogin()
 #cap.release()
