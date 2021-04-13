5476a109f6af7d211d89b2816715a66f6615559f
Hung Nguyen <hunglnl01@gmail.com>
2021-04-13 09:26:29 -0700

    add more tests

--- RPI/Tests/test_renderingUtil.py
+++ RPI/Tests/test_renderingUtil.py
@@ -7,11 +7,14 @@ import matplotlib.image as mpimg
 
 workingDir = dut.workingDir
 
+#test if the image resize works
 class Test(TestCase):
     def test_resize_image(self):
         for filename in os.listdir(workingDir + "/images"):
             img = dut.resizeImage("/images/" + filename)
-            # imgplot = plt.imshow(img)
-            # plt.show()
+            imgplot = plt.imshow(img)
+            plt.show()
+            
+            # test if the image is smaller than expected size
             assert img.width < 500 and img.height < 500
             img.close()
